5b9b8acf1ad8d968642afe8e5783fe8bf426e953
vaishali <55455022+vaishali998@users.noreply.github.com>
2020-04-29 22:07:56 +0530

    Created AnswerController with all function

--- stackoverflow-api/src/main/java/com/upgrad/stackoverflow/api/controller/AnswerController.java
+++ stackoverflow-api/src/main/java/com/upgrad/stackoverflow/api/controller/AnswerController.java
@@ -1,3 +1,34 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@vaishali998 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+vaishali998
+/
+Stackoverflow-backend
+0
+00
+ Code
+ Pull requests 0 Actions
+ Projects 0
+ Wiki
+ Security 0
+ Insights
+ Settings
+Stackoverflow-backend/stackoverflow-api/src/main/java/com/upgrad/stackoverflow/api/controller/AnswerController.java /
+@vaishali998 vaishali998 implemented answerController
+25d0ea8 3 days ago
+@vaishali998@Muskaanpal05
+112 lines (104 sloc)  6.18 KB
+  
 package com.upgrad.stackoverflow.api.controller;
 
 import com.upgrad.stackoverflow.api.model.*;
@@ -19,7 +50,8 @@ import java.util.List;
 import java.util.UUID;
 
 
-@RequestMapping("/")
+@RequestMapping("")
+@RestController
 public class AnswerController {
 
 
@@ -36,7 +68,19 @@ public class AnswerController {
      * @throws AuthorizationFailedException
      * @throws InvalidQuestionException
      */
-
+    @PostMapping("/question/{questionId}/answer/create")
+    public ResponseEntity<AnswerResponse> postAnswer(@RequestBody AnswerRequest answerRequest, @RequestHeader String authorization, @PathVariable String questionId) throws AuthorizationFailedException, InvalidQuestionException
+    {
+        AnswerEntity answerEntity=new AnswerEntity();
+        answerEntity.setUuid(uuid.randomUuid().toString());
+        answerEntity.setAns(answerRequest.getAns());
+        answerEntity.setDate(ZoneDateTime.now());
+        QuestionEntity questionEntity = answerBusinessService.getQuestionByUuid(questionId);
+        answerRequest.setQuestion(questionEntity);
+        AnswerEntity answerEntity1 = answerBusinessService.createAnswer(answerEntity,authorization);
+        AnswerResponse answerResponse = new AnswerResponse().id(answerEntity.getUuid()).status("Answer Created");
+        return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.CREATED);
+    }
     /**
      * A controller method to edit an answer in the database.
      *
@@ -47,16 +91,31 @@ public class AnswerController {
      * @throws AuthorizationFailedException
      * @throws AnswerNotFoundException
      */
-
-    /**
-     * A controller method to delete an answer in the database.
-     *
-     * @param answerId      - The uuid of the answer to be deleted in the database.
-     * @param authorization - A field in the request header which contains the JWT token.
-     * @return - ResponseEntity<AnswerDeleteResponse> type object along with Http status OK.
-     * @throws AuthorizationFailedException
-     * @throws AnswerNotFoundException
-     */
+     @PutMapping("/answer/edit/{answerId}")
+    public ResponseEntity<AnswerEditResponse> editAnswer(@RequestBody AnswerEditRequest answerEditRequest, @RequestHeader String authorization, @PathVariable String answerId) throws AuthorizationFailedException, AnswerNotFoundException
+     {
+         AnswerEntity answerEntity=new AnswerEntity();
+         answerEntity.setAns(answerEditRequest.getAns());
+         AnswerEntity answerEntity1 = answerBusinessService.editAnswerContent(answerEntity,  answerId,  authorization);
+         AnswerEditResponse answerEditResponse =new AnswerEditResponse().id(answerEntity.getUuid()).status("Answer Edited");
+         return new ResponseEntity<AnswerEditResponse>(answerEditResponse, HttpStatus.EDITED);
+     }
+      /**
+ * A controller method to delete an answer in the database.
+ *
+ * @param answerId      - The uuid of the answer to be deleted in the database.
+ * @param authorization - A field in the request header which contains the JWT token.
+ * @return - ResponseEntity<AnswerDeleteResponse> type object along with Http status OK.
+ * @throws AuthorizationFailedException
+ * @throws AnswerNotFoundException
+ */
+     @DeleteMapping("/answer/delete/{answerId}")
+    public ResponseEntity<AnswerDeleteResponse> deleteAnswer(@PathVariable String answerId, @RequestHeader("authorization") String authorization) throws AuthorizationFailedException, AnswerNotFoundException
+     {
+         AnswerEntity answerEntity = answerBusinessService.deleteAnswer(answerId, authorization);
+         AnswerDeleteResponse answerDeleteResponse = new AnswerDeleteResponse().id(answerEntity.getUuid()).status("Answer Deleted");
+         return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse,HttpStatus.DELETED);
+     }
 
     /**
      * A controller method to fetch all the answers for a specific question in the database.
@@ -67,5 +126,19 @@ public class AnswerController {
      * @throws AuthorizationFailedException
      * @throws InvalidQuestionException
      */
-
+      @GetMapping("/answer/all/{questionId}")
+    public ResponseEntity<List<AnswerDetailsResponse>> getAllByquestionId(@PathVariable String questionId, @RequestHeader("auhtorization") String authorization) throws AuthorizationFailedException, InvalidQuestionException
+      {
+          TypedQuery<AnswerEntity> answerList = answerBusinessService.getAnswersByQuestion(questionId, authorization);
+          List<AnswerEntity> resultList = answerList.getResultList();
+          List<AnswerDetailsResponse> responseList = resultList.stream()
+                  .map(answer -> {
+                      AnswerDetailsResponse response = new AnswerDetailsResponse();
+                      response.setAns(answer.getAns());
+                      response.setId(answer.getUuid());
+                      return response;
+                  }).collect(Collectors.toList());
+          return new ResponseEntity<>(responseList, HttpStatus.OK);
+      }
 }
+
